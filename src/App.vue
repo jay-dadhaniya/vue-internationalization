6d2beba5ebb0ab67432ec8756315a30c82220b2f
jaydadhaniya <jay.dadhaniya@bacancy.com>
2023-09-01 17:00:02 +0530

    Added vue i18n plugin

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloI18n/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HelloI18n from './components/HelloI18n.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    HelloI18n
   }
 }
 </script>
---
+++ src/components/HelloI18n.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h4>{{ $t('message') }}</h4>
+    <h4>{{ $t('hello') }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloI18n'
+}
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello i18n in SFC!"
+  }
+}
+</i18n>
