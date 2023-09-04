a61d49d9ce48ee35bdf5bacc6bf22fcd4e7e7903
jaydadhaniya <jay.dadhaniya@bacancy.com>
2023-09-04 16:59:59 +0530

    Added nested messages

--- src/components/HelloI18n.vue
+++ src/components/HelloI18n.vue
@@ -4,7 +4,7 @@
       {{ $t('language_select_info') }}
     </h1>
     <div>
-      <select v-model="selectedLanguage">
+      <select v-model="selectedLanguage" class="minimal">
         <option v-for="lang in languages" :key="lang" :value="lang">
           {{ lang }}
         </option>
@@ -17,6 +17,23 @@
 
     <h4>{{ $t('message') }}</h4>
     <h4>{{ $t('hello') }}</h4>
+
+    <div class="hierarchy">
+      <ul>
+        <li>
+          {{ $t("parent") }}
+          <ul>
+            <li>{{ $t("child") }} 1</li>
+            <li>
+              {{ $t("child") }} 2
+              <ul>
+                <li>{{ $t("parent.child.inner_child") }}</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -67,13 +84,83 @@ export default {
 <i18n>
 {
   "en": {
-    "hello": "Hello i18n in SFC!"
+    "hello": "Hello i18n in SFC!",
+    "parent": "parent",
+    "child": "child"
   },
   "hi": {
-    "hello": "एसएफसी में नमस्ते i18n!"
+    "hello": "एसएफसी में नमस्ते i18n!",
+    "parent": "माता-पिता",
+    "child": "बालक"
   },
   "fr": {
-    "hello": "Bonjour i18n dans SFC"
+    "hello": "Bonjour i18n dans SFC",
+    "parent": "parent",
+    "child": "enfant"
   }
 }
 </i18n>
+
+
+<style scoped>
+select {
+  /* styling */
+  background-color: white;
+  border: thin solid blue;
+  border-radius: 4px;
+  display: inline-block;
+  font: inherit;
+  line-height: 1.2em;
+  padding: 0.5em 3.5em 0.5em 1em;
+
+  /* reset */
+  margin: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
+
+select.minimal {
+  background-image: linear-gradient(45deg, transparent 50%, gray 50%),
+    linear-gradient(135deg, gray 50%, transparent 50%),
+    linear-gradient(to right, #ccc, #ccc);
+  background-position: calc(100% - 20px) calc(1em + 2px),
+    calc(100% - 15px) calc(1em + 2px), calc(100% - 2.5em) 0.5em;
+  background-size: 5px 5px, 5px 5px, 1px 1.5em;
+  background-repeat: no-repeat;
+}
+
+select.minimal:focus {
+  background-image: linear-gradient(45deg, green 50%, transparent 50%),
+    linear-gradient(135deg, transparent 50%, green 50%),
+    linear-gradient(to right, #ccc, #ccc);
+  background-position: calc(100% - 15px) 1em, calc(100% - 20px) 1em,
+    calc(100% - 2.5em) 0.5em;
+  background-size: 5px 5px, 5px 5px, 1px 1.5em;
+  background-repeat: no-repeat;
+  border-color: green;
+  outline: 0;
+}
+
+select:-moz-focusring {
+  color: transparent;
+  text-shadow: 0 0 0 #000;
+}
+
+h1 {
+  line-height: 100%;
+  margin: 0 auto 2rem auto;
+  max-width: 30rem;
+  font-size: larger;
+}
+
+.hierarchy {
+  line-height: 100%;
+  margin: 0 auto 2rem auto;
+  max-width: 15rem;
+  font-size: larger;
+  text-align: justify;
+}
+</style>
